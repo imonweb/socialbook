3a8dd995a43e7c6d94b3f821310d13eb226b320a
Imon <contact@imonweb.co.uk>
2023-04-29 21:05:56 +0100

    local storage

--- app.js
+++ app.js
@@ -16,4 +16,26 @@ darkBtn.onclick = function() {
   darkBtn.classList.toggle("dark-btn-on");
 
   document.body.classList.toggle("dark-theme")
+
+  // local storage update
+  if(localStorage.getItem("theme") == "light"){
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 }
+
+/*  Local Storage */
+
+if(localStorage.getItem("theme") == "light"){
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if(localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
+
+localStorage.setItem("theme", "dark");
+localStorage.getItem("theme");
