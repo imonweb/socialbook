8ba2a2a96d2f14ad713aec1869586b66530c72cc
Imon <contact@imonweb.co.uk>
2023-04-29 10:22:18 +0100

    toggle menu

---
+++ app.js
@@ -0,0 +1,12 @@
+// const settingsMenu = document.querySelector('.settings-menu');
+// const online = document.querySelector('.online');
+
+// online.addEventListener('click', () => {
+//   console.log('clicked');
+// })
+
+const settingsMenu = document.querySelector('.settings-menu');
+
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
+}
